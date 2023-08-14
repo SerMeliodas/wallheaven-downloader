ff270fe6b5b55b66ec696176c652d6dacb06d7aa
SerMeliodas <oliver9574@gmail.com>
2023-08-14 22:00:28 +0200

    added progress bar

--- walldownloader.py
+++ walldownloader.py
@@ -2,9 +2,11 @@ import os
 import sys
 import pathlib
 import requests
+from clint.textui import progress
 from multiprocessing import Process
 
 
+CHUNK_SIZE = 1024
 PROCESSES_COUNT = 4
 SAVE_PATH = f'{pathlib.Path.home()}/Pictures/wallpapers/'
 API_URLS = {
@@ -29,10 +31,17 @@ def divide_chunks(array: list, chunk_size: int):
 
 def download_images(images: list):
     for image in images:
-        data = requests.get(image['path'])
+        data = requests.get(image['path'], stream=True)
         download_path = f'{SAVE_PATH}{get_save_id()}{get_ext(image["path"])}'
-        open(download_path, 'wb').write(data.content)
-        print(f"Downloaded:  {os.getpid()}:{image['path']}")
+        length = int(data.headers.get('content-length'))
+        with open(download_path, 'wb') as file:
+            for chunk in progress.bar(data.iter_content(chunk_size=CHUNK_SIZE),
+                                      expected_size=length/CHUNK_SIZE + 1,
+                                      label=f'Downloading: {os.getpid()}:{image["id"]}',
+                                      ):
+                if chunk:
+                    file.write(chunk)
+                    file.flush()
 
 def create_download_processes(images: list):
     for chunk in divide_chunks(images, PROCESSES_COUNT):
