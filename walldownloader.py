9cdd60cbe7ac9990e860d82f3828f8d5ee621a56
SerMeliodas <oliver9574@gmail.com>
2023-08-15 14:10:48 +0200

    removed multiprocessing

--- walldownloader.py
+++ walldownloader.py
@@ -1,13 +1,11 @@
 import os
-import sys
 import pathlib
 import requests
+from clint import arguments
 from clint.textui import progress
-from multiprocessing import Process
 
 
 CHUNK_SIZE = 1024
-PROCESSES_COUNT = 4
 SAVE_PATH = f'{pathlib.Path.home()}/Pictures/wallpapers/'
 API_URLS = {
     'search' : 'https://wallhaven.cc/api/v1/search',
@@ -25,36 +23,29 @@ def get_ext(url):
     ext = os.path.splitext(url)[1]
     return ext
 
-def divide_chunks(array: list, chunk_size: int):
-    for i in range(0, len(array), chunk_size):
-        yield array[i:i+chunk_size]
-
 def download_images(images: list):
     for image in images:
+
         data = requests.get(image['path'], stream=True)
         download_path = f'{SAVE_PATH}{get_save_id()}{get_ext(image["path"])}'
         length = int(data.headers.get('content-length'))
+
         with open(download_path, 'wb') as file:
+
             for chunk in progress.bar(data.iter_content(chunk_size=CHUNK_SIZE),
                                       expected_size=length/CHUNK_SIZE + 1,
-                                      label=f'Downloading: {os.getpid()}:{image["id"]}',
-                                      ):
+                                      label=f'Downloading: {image["id"]}'):
                 if chunk:
                     file.write(chunk)
                     file.flush()
 
-def create_download_processes(images: list):
-    for chunk in divide_chunks(images, PROCESSES_COUNT):
-        process = Process(target=download_images, args=(chunk,))
-        process.start()
-        process.join()
-
 def main():
-    if len(sys.argv) == 1:
+    args = arguments.Args()
+    if len(args.all) == 0:
         print('Please specify the search query')
     else:
-        images = get_images(sys.argv[1])
-        create_download_processes(images)
+        images = get_images(args.get(0))
+        download_images(images)
 
 
 if __name__ == "__main__":
